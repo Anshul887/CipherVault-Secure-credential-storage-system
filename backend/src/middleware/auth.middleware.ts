a62cf0b25d0dca18ce541ddb3359dde2ec9acde7
Anshul Mankar <anshulmankar887@gmail.com>
2026-06-13 10:55:45 +0530

    Update auth.middleware.ts

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -1,55 +1,63 @@
+import jwt from "jsonwebtoken";
+
 import {
   Request,
   Response,
   NextFunction
 } from "express";
 
-import jwt from "jsonwebtoken";
-
 export interface AuthRequest
   extends Request {
 
   userId?: string;
+
+  role?: string;
 }
 
-export const authMiddleware = (
+export const authMiddleware =
+(
   req: AuthRequest,
   res: Response,
   next: NextFunction
 ) => {
 
-  const authHeader =
-    req.headers.authorization;
+  const token =
+    req.headers.authorization
+      ?.split(" ")[1];
 
-  if (!authHeader) {
+  if (!token) {
 
-    return res.status(401).json({
-      message: "Unauthorized"
-    });
+    return res.status(401)
+      .json({
+        message:
+          "Unauthorized"
+      });
 
   }
-
-  const token =
-    authHeader.split(" ")[1];
 
   try {
 
     const decoded =
       jwt.verify(
         token,
-        process.env.JWT_SECRET as string
+        process.env.JWT_SECRET!
       ) as any;
 
     req.userId =
       decoded.userId;
 
+    req.role =
+      decoded.role;
+
     next();
 
   } catch {
 
-    return res.status(401).json({
-      message: "Invalid token"
-    });
+    return res.status(401)
+      .json({
+        message:
+          "Invalid Token"
+      });
 
   }
 };
