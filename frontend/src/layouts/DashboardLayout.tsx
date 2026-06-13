f4a726eba723012eab4b61efb739c0cab766b170
Anshul Mankar <anshulmankar887@gmail.com>
2026-06-13 11:24:56 +0530

    Create DashboardLayout.tsx

---
+++ frontend/src/layouts/DashboardLayout.tsx
@@ -0,0 +1,30 @@
+import {
+ Outlet
+}
+from "react-router-dom";
+
+import Sidebar
+from "../components/Sidebar";
+
+export default function
+DashboardLayout(){
+
+ return(
+
+ <div
+ className=
+ "flex">
+
+  <Sidebar/>
+
+  <main
+  className=
+  "flex-1 p-6">
+
+   <Outlet/>
+
+  </main>
+
+ </div>
+ );
+}
